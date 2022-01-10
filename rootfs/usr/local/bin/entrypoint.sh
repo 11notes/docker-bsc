1d237a709ac507cd3c5df259e160b94ff1d9d947
11notes <github@11notes.ch>
2022-01-10 06:49:07 +0100

    full node

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/ash
 if [ "$1" = "geth" ]; then
-    if [ ! -d /bsc/var/geth/chaindata ]; then
-        echo "init bsc ..."
-        geth --datadir "/bsc/var" init /bsc/etc/genesis.json
-    fi
-
     echo "starting bsc ..."
-    set -- "$@" --datadir "/bsc/var" --config "/bsc/etc/default.toml" --syncmode fast --diffsync --cache 8000 --rpc.allow-unprotected-txs --txlookuplimit 0 # default from binance git
+    set -- "$@" --datadir "/bsc/var" --config "/bsc/etc/config.toml" --diffsync --cache 16384 --rpc.allow-unprotected-txs --txlookuplimit 0
 fi
 
 exec "$@"
