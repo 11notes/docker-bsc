e599c7550422f9bd5e949b11325e3622dc046de3
11notes <github@11notes.ch>
2022-02-11 15:34:21 +0100

    update 1.1.8

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,4 +1,3 @@
-#!/bin/ash
 if [ "$1" = "geth" ]; then
     echo "starting bsc ..."
     set -- "$@" \
@@ -8,15 +7,8 @@ if [ "$1" = "geth" ]; then
         --cache 16384  \
         --rpc.allow-unprotected-txs  \
         --txlookuplimit 0 \
-        --http \
-            --http.addr 0.0.0.0 \
-            --http.api personal,eth,net,web3 \
-            --http.corsdomain '*' \
         --ws \
             --ws.addr 0.0.0.0 \
-            --ws.api eth,net,web3 \
-            --ws.origins '*' \
-        --ipcdisable
+            --ws.api eth,web3 \
+            --ws.origins '*'
 fi
-
-exec "$@"
